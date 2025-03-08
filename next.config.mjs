fd1274b4dfa0548230d82989664feb6088e8d4bc
premv <premchand@capitalnumbers.com>
2025-03-08 15:03:37 +0530

    updated payload size

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  api: {
+    bodyParser: {
+      sizeLimit: "200mb",
+    },
+  },
   async headers() {
     return [
       {
-        source: '/:path*',
+        source: "/:path*",
         headers: [
           {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
           },
           {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
           },
         ],
       },
